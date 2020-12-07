8988216d036f520025911a4c5f25b0cc00c17a5c
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2020-12-07 12:59:52 -0600

    Prints JSON response object to stdout

--- piggy_back_trading_bot.cpp
+++ piggy_back_trading_bot.cpp
@@ -9,6 +9,23 @@ using json = nlohmann::json;
 #define STR(X) #X
 #define STRINGIZE_VAL(X) STR(X)
 
+
+
+// Writes JSON response object to stdout
+static size_t write_data(const char* in, std::size_t size, std::size_t num, char* out) {
+
+	std::string data(in, size*num);
+
+    auto market_data_json = json::parse(data);
+
+    std::cout << "JSON DUMP: " << std::endl;
+    std::cout << market_data_json.dump(4) << std::endl;
+
+    std::cout << market_data_json["ticker"] << std::endl;
+
+	return size * num;
+}
+
 int main() {
 
 #if defined (APIKEYID) &&  defined (APISECRETKEY)
@@ -19,45 +36,40 @@ int main() {
 
 
 	std::cout << "request: " + request << std::endl;
+
+    // Set HTTP header
 	CURL *curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_URL, request.c_str());
 	curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
 	curl_easy_setopt(curl, CURLOPT_TIMEOUT,20);
 	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-	struct curl_slist *chunk = NULL;
-	std::string keyid = "APCA-API-KEY-ID: " STRINGIZE_VAL(APIKEYID);
-	std::string secret = "APCA-API-SECRET-KEY: " STRINGIZE_VAL(APISECRETKEY);
 
-    std::cout << secret << std::endl;
-	chunk = curl_slist_append(chunk, keyid.c_str());
-	chunk = curl_slist_append(chunk, secret.c_str());
+    // Set keys needed for API to authenticate 
+	struct curl_slist *keys = NULL;
+	keys = curl_slist_append(keys, "APCA-API-KEY-ID: " STRINGIZE_VAL(APIKEYID));
+	keys = curl_slist_append(keys, "APCA-API-SECRET-KEY: " STRINGIZE_VAL(APISECRETKEY));
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, keys);
 
-	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
-
-	//Response information
-	long httpCode(0);
-	std::string httpData;
+	// Response information
+	unsigned http_code = 0;
+	std::string http_data;
 	
-	
-
-    //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
     
-	//curl_easy_setopt(curl, CURLOPT_WRITEDATA, &httpData);
-
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &http_data);
 
 	curl_easy_perform(curl);
 
-	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
 	curl_easy_cleanup(curl);
 
-	std::cout << "httpCode: " << httpCode << std::endl;
-    std::cout << "httpdata: " << httpData << std::endl;
+
+    return 0;
 
 #else   
     std::cerr << "API-KEY-ID and/or API-SECRET-KEY are/is undefined."  << std::endl;
     std::cerr << "Usage: >> $ make -DAPIKEYID={YOUR_API_KEY_ID} -DAPISECRETKEY={YOUR_API_SECRET_KEY}"  << std::endl;
-    return 0;
+    return 1;
 #endif
 }
