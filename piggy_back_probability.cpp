11bb5a277adce3dca0819cc3f65dc239cb78e333
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2021-01-05 13:44:15 -0600

    Scalping stat for stop-loss when price reaches %1 below last low

--- piggy_back_probability.cpp
+++ piggy_back_probability.cpp
@@ -372,7 +372,9 @@ std::cout << month+1 << "/" << day << "/" << year << std::endl;
 
                                     float new_minute_high_price = stock_spiked_json["results"][new_minute_indx]["h"];
                                     float new_minute_low_price = stock_spiked_json["results"][new_minute_indx]["l"];
-
+                                    
+                                    breakpoint_low_price -= (breakpoint_low_price*0.01);
+                                    
                                     if(new_minute_low_price < breakpoint_low_price) {
 
                                         worst_case_sell_price = new_minute_low_price;
